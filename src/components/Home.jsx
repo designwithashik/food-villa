269d463e7b1d7bd6484624b0f7ceaf8f9e9304eb
Ashik <designwithashik@gmail.com>
2023-04-06 15:21:35 +0600

    Animation Added to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
+import Lottie from 'lottie-react';
 import React from 'react';
-
+import food from '../assets/25523-wok-pan-food-fry-on-fire.json'
 const Home = () => {
     return (
-        <div>
-            <h1 className='text-3xl'>Order food from favourite restaurants near you.
+        <div className='flex items-center'>
+            <div>
+            <h1 className='text-3xl font-bold'>Order food from favourite restaurants near you.
 </h1>
+            </div>
+            <Lottie animationData={food} loop={true} />
         </div>
     );
 };
